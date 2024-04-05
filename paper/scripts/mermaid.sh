96e96296e3328e9526607d749226288c0eb19b9d
Nik Benson <nbenson@nettrek.de>
2024-04-05 12:21:14 +0200

    wip

--- paper/scripts/mermaid.sh
+++ paper/scripts/mermaid.sh
@@ -3,4 +3,7 @@
 caller_script_path="$(pwd)"
 user_path="$1"
 user_path="${user_path%*/}"
-mmdc -i "$caller_script_path/$user_path/img/diagrams/"**.mmd -t default -b transparent -e png
+
+for file in "$caller_script_path/$user_path/img/diagrams/"**.mmd; do
+  mmdc -i "$file" -t default -b transparent -e png
+done
