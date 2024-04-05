cb20d44fc5094aec04a54109b69551a5e9f3d18d
Nik Benson <nbenson@nettrek.de>
2024-04-05 12:21:14 +0200

    wip

---
+++ paper/assets/code/c/compiler.c
@@ -0,0 +1,43 @@
+void print_ast(AST_NODE *node) {
+    switch (node->type) {
+        case t_OBJECT: {
+            printf("{");
+
+            for (int i = 0; i < node->children_count; ++i) {
+                print_ast(node->children[i]);
+                if (i != node->children_count - 1)
+                    printf(", ");
+            }
+
+            printf("}");
+            break;
+        }
+        case t_ELEMENT: {
+            bool is_first_key = node->children[0]->type == t_KEY;
+            print_ast(node->children[is_first_key ? 0 : 1]);
+            printf(": ");
+            print_ast(node->children[is_first_key ? 1 : 0]);
+            break;
+        }
+        case t_KEY: {
+            printf("\"%s\"", node->value.s);
+            break;
+        }
+        case t_VALUE: {
+            print_ast(node->children[0]);
+            break;
+        }
+        case t_BOOL: {
+            printf("%s", node->value.b ? "true" : "false");
+            break;
+        }
+        case t_INT: {
+            printf("%d", node->value.i);
+            break;
+        }
+        case t_FLOAT: {
+            printf("%f", node->value.f);
+            break;
+        }
+    }
+}
